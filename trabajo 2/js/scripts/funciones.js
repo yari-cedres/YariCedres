94b289e63b01cc8499051edfdc1b5844afdbc2a4
yari-cedres <yari-cedres@hotmail.com>
2022-04-09 11:38:08 -0300

    commit

--- trabajo 2/js/scripts/funciones.js
+++ trabajo 2/js/scripts/funciones.js
@@ -1,8 +1,23 @@
 function saludar() {
-    alert("hola mundo");
+    alert("hola");
 }
 saludar();
 function chau() {
     alert("chau");
 }
 chau();
+function mensaje() {
+    alert("pasaste por aca");
+}
+function cambiar(){
+    document.getElementById("a1").style.color="red";
+    document.getElementById("a1").style.background="green";
+    
+
+}
+function volver(){
+    document.getElementById("a1").style.color="black";
+    document.getElementById("a1").style.background="blue";
+    
+
+}
